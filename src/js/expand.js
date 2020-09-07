5d508066acd75457be33701f9b673f5053216854
Yuhymenko Dmytro <d.yuhymenko@riverstart.ru>
2020-09-08 01:50:10 +0300

    feat: new section designers + fixed some bugs

--- src/js/expand.js
+++ src/js/expand.js
@@ -42,10 +42,47 @@ export default function expand() {
             headerExpand.dataset.isopen = 'true';
             $('.header__expand').fadeIn();
 
+            document.querySelectorAll('.expand__collapsible').forEach(elem => {
+                if (elem.dataset.isopen == 'true') {
+                    const content = elem.nextElementSibling;
+
+                    content.style.maxHeight = content.scrollHeight + 'px';
+                    elem.style.backgroundImage = `url("${elem.dataset.bg}")`;
+                    elem.style.backgroundPosition = `72% 92%`;
+                    elem.style.color = '#ffffff';
+                    // event.target.style.backgroundColor = 'rgba(#ffffff, 0)';
+                    // console.log(event.target.dataset.bg);
+
+                    const expandSwiper = new Swiper('.expand__swiper-container', {
+                        slidesPerView: 'auto',
+                        slidesOffsetAfter: window.innerWidth / 2,
+                        updateOnWindowResize: false,
+                        scrollbar: {
+                            el: '.expand__swiper-scrollbar',
+                        },
+
+                    });
+
+                    if (window.innerWidth >= 1280) {
+                        $('.expand__swiper-wrapper').addClass('swiper-disabled');
+                        // $('.expand__swiper-scrollbar').addClass("disabled");
+                        document.querySelectorAll('.expand__swiper-container').forEach(elem => {
+                            elem.style.opacity = '0';
+                        });
+                        content.style.opacity = '1';
+
+                    } else {
+                        $('.expand__swiper-wrapper').removeClass('swiper-disabled');
+                    }
+                }
+            });
+
         } else {
             $('.header__expand').fadeOut();
             headerExpand.dataset.isopen = 'false';
         }
+
+
     });
 
     const headerSwiper = $('.expand__swiper-container');
@@ -67,13 +104,14 @@ export default function expand() {
                 elem.style.backgroundPosition = `72% -200px`;
                 elem.style.color = '#272727';
             });
-            content.style.maxHeight = content.scrollHeight + 'px';
 
+            // content.style.opacity = '1';
+            content.style.maxHeight = content.scrollHeight + 'px';
             event.target.style.backgroundImage = `url("${event.target.dataset.bg}")`;
             event.target.style.backgroundPosition = `72% 92%`;
             event.target.style.color = '#ffffff';
             // event.target.style.backgroundColor = 'rgba(#ffffff, 0)';
-            console.log(event.target.dataset.bg);
+            // console.log(event.target.dataset.bg);
 
             const expandSwiper = new Swiper('.expand__swiper-container', {
                 slidesPerView: 'auto',
@@ -90,6 +128,10 @@ export default function expand() {
             if (window.innerWidth >= 1280) {
                 $('.expand__swiper-wrapper').addClass('swiper-disabled');
                 // $('.expand__swiper-scrollbar').addClass("disabled");
+                document.querySelectorAll('.expand__swiper-container').forEach(elem => {
+                    elem.style.opacity = '0';
+                });
+                content.style.opacity = '1';
 
             } else {
                 $('.expand__swiper-wrapper').removeClass('swiper-disabled');
