620857834483f8e59b0cafa2a8f0bbd42b869501
Yuhymenko Dmytro <d.yuhymenko@riverstart.ru>
2020-09-08 18:26:39 +0300

    feat: new section designers + fixed some bugs

--- webpack.config.js
+++ webpack.config.js
@@ -13,21 +13,23 @@ const webpackConfig = {
         publicPath: '/assets/js/',
     },
 
+
+
     module: {
         rules: [
             // Не смог сделать, чтоб показывал ошибки, но собирал дальше
-//            {
-//                enforce: 'pre',
-//                test: /\.js$/,
-//                exclude: /(node_modules|bower_components|libs)/,
-//                loader: 'eslint-loader',
-//                options: {
-//                    emitError: false,
-//                    emitWarning: false,
-//                    failOnError: false,
-//                    failOnWarning: false,
-//                },
-//            },
+            //            {
+            //                enforce: 'pre',
+            //                test: /\.js$/,
+            //                exclude: /(node_modules|bower_components|libs)/,
+            //                loader: 'eslint-loader',
+            //                options: {
+            //                    emitError: false,
+            //                    emitWarning: false,
+            //                    failOnError: false,
+            //                    failOnWarning: false,
+            //                },
+            //            },
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
